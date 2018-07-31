0ebaa8b4ebaa701df8bb0260f3c4e59d1271c261
tau150 <tau150@hotmail.com>
2018-07-31 08:31:20 -0300

    changed email for sistemasdpe

--- utils/email-util.js
+++ utils/email-util.js
@@ -9,9 +9,9 @@ exports.sendEmail = async (recipient, password) => {
   var data = {
     from:
       "Dirección Pcial. Estadística <no-responder@estadistica.ec.gba.gov.ar>",
-    to: recipient,
+    to: "sistemasdpe@gmail.com",
     subject: "Registro exitoso",
-    html: `<html> <h3> Fuiste registrado con éxito  </h3><br> <p>Podés ingresar al sistema con tu email y el siguiente password <strong> ${password} </strong>, te aconsejamos cambiarlo para mayor seguridad.</p>
+    html: `<html> <h3> Fuiste registrado con éxito  </h3><br> <p>Podés ingresar al sistema con tu email ${recipient} y el siguiente password <strong> ${password} </strong>, te aconsejamos cambiarlo para mayor seguridad.</p>
     <br><br><br>
     <div><img src="cid:pap-inst.png" style="width: 100%;"/></div>
     </html>`,
@@ -33,9 +33,9 @@ exports.sendRecoverEmail = async (recipient, password) => {
     var data = {
       from:
         "Dirección Pcial. Estadística <no-responder@estadistica.ec.gba.gov.ar>",
-      to: recipient,
+      to: "sistemasdpe@gmail.com",
       subject: "Reseteo de clave",
-      html: `<html> <h3> Generación de Nueva Clave </h3><br> <p> El proceso de reseteo fue realizado con éxito, podés ingresar con tu email y la siguiente contraseña <strong> ${password} </strong>, te aconsejamos cambiarlo para mayor seguridad.</p> <br><br><br>  <div><img src="cid:pap-inst.png" style="width: 100%;"/></div> </html>`,
+      html: `<html> <h3> Generación de Nueva Clave </h3><br> <p> El proceso de reseteo fue realizado con éxito, podés ingresar con tu email ${recipient} y la siguiente contraseña <strong> ${password} </strong>, te aconsejamos cambiarlo para mayor seguridad.</p> <br><br><br>  <div><img src="cid:pap-inst.png" style="width: 100%;"/></div> </html>`,
       inline: filename
     };
 
